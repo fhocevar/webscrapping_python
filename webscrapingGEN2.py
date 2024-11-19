411fe9f0e2a2b92ee2ba355b52f119cb58533267
Fernando <fhocevar@gmail.com>
2024-11-18 21:42:25 -0300

    add de mais empresas de outros setores

--- webscrapingGEN2.py
+++ webscrapingGEN2.py
@@ -29,7 +29,7 @@ def consulta_empresas(empresa_list):
         nome_empresa_codificado = quote(nome_empresa)  # Codifica o nome da empresa para a URL
         url = f'https://www.google.com/search?q={nome_empresa_codificado}'
         driver.get(url)
-        time.sleep(3)  # Aguarda o carregamento da página
+        time.sleep(5)  # Aguarda o carregamento da página
 
         # Coleta informações dos resultados da busca
         try:
@@ -69,7 +69,7 @@ def consulta_empresas(empresa_list):
     driver.quit()  # Fecha o navegador
 
     # Salvando os dados em um arquivo CSV
-    with open('empresas130_testando3.csv', mode='w', newline='', encoding='utf-8') as file:
+    with open('empresas130_testando4.csv', mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Nome', 'Endereço', 'Telefone', 'Celular', 'Website'])  # Cabeçalho
         writer.writerows(dados_empresas)  # Escreve os dados das empresas
@@ -84,30 +84,34 @@ empresas = [
     "Distribuidora NutriCoop", "Casa Flora", "Giroil", "SuperFoods Brasil", "LeManjue Organics Distribuidora", "Bella Mix Produtos Naturais",
     "Castanha Mix Distribuidora", "Nutri Import", "Gran Nuts", "Litoral Produtos Naturais", "Atacado da Castanha", "Distribuidora Hortelã",
     "Comercial Vitória", "Nutsland", "Brasil Nuts", "Central de Grãos e Castanhas", "Empório Terra Madre", "Atacado Frutos da Terra",
-    "Mundo dos Grãos", "Distribuidora Viva Alimentos Naturais", "Simples & Natural - Grãos e Castanhas", "Nutty Bavarian", "Nutty Bavarian",
-    "Noz do Brasil - Produtos Naturais - Vila Prudente SP", "Nutty Bavarian", "Nutty Up", "Empório das Castanhas", "M.J. Doces E Castanhas",
-    "Supermercado Castanha", "Mercadão Natural", "Empório das Castanhas", "Pedro das Castanhas", "Empório Zanata", "Nutty Bavarian",
+    "Mundo dos Grãos", "Distribuidora Viva Alimentos Naturais", "Simples & Natural - Grãos e Castanhas", "Noz do Brasil - Produtos Naturais - Vila Prudente SP",
+    "Nutty Up", "Empório das Castanhas", "M.J. Doces E Castanhas", "Supermercado Castanha", "Mercadão Natural", "Empório Zanata",
     "Empório Santa Castanha", "Brazilian Nuts - Açaí, sucos e castanhas - Vila Madalena, São Paulo", "Armazém Paes e Paes Zona Cerealista do ABC",
-    "Nutty Bavarian", "EMPÓRIO SETE MESTRES", "Nutty Bavarian", "Noz do Brasil - Produtos Naturais - Vila Prudente SP", "Macadâmia Cerealista LTDA",
-    "MACADÂMIA MERCADO SAUDÁVEL", "Macadamia Bolsas e Acessórios", "Macadâmia Home", "Macadâmia moda feminina", "Mr. Cheney - Cookies",
-    "Biscoitê A Biscoiteira", "Dadiva Natural - Loja de Produtos Naturais Santo André", "Macadâmia Bela Vista", "Restaurante Bananeira",
-    "Pecan Restaurante - Shop. Paineiras", "Residencial Pecan", "Pecan Móveis Planejados", "Loteamento Fechado Pecan (Lotes a partir de 360 M2)",
-    "Pecan Store", "Caju", "Pé de Caju Bar & Petiscaria", "Caju Bar", "CaJu Lanches e Salgados", "Boteco do Juca", "Caju", "Empório Caju - Cerealista",
-    "Restaurante do Caju", "Lanchonete e Restaurante Caju Verde", "Restaurante Caju Verde", "Lanchonete e Restaurante Caju", "Tapiocas e pastéis gourmet do cajú",
-    "Caju", "Vila Caju", "Lanchonete Nova Skina Caju Verde", "Lanchonete e Restaurante Toca do Caju", "Maria Caju", "Caju Bar", "CAJU MEL DOCES",
-    "Caju açai e lanches", "Atacadão - São Bernardo Centro", "Vencedor Atacadista", "Assaí Atacado e Varejo", "Assaí Atacadista", "Atacado Bem Barato",
-    "Tenda Atacado - Diadema", "Atento atacadista", "Grupo Bem Barato", "Oba Hortifruti Farm - Mercado, Frutas, Verduras, Legumes, Açougue e Adega",
-    "Atacado de folheados", "Mega Boi", "Choco Art Artigos Para Festas/ distribuidora / doces / confeitaria /atacado / varejo / essências / artesanato / bisqui",
-    "Mafat atacado de roupas semi novas e variedades", "Coco Atacado E Varejo", "L Modas Jeans Atacado e Varejo", "Suplemercado FIT supplements São Bernardo Wholesale & Retail",
-    "La Bella Semijóias - Atacado de Semijóias", "Nova Veneza - Atacado e Varejo", "Brasileirão Atacado", "Grupo Bem Barato", "Oba Hortifruti Farm - Mercado, Frutas, Verduras, Legumes, Açougue e Adega",
-    "Mercado Vila Nova", "Grupo Bem Barato", "Mercado RV", "Mercado Loyola", "Mercado DuParque ll", "Extra Mercado", "Mercado Lucas", "May 13",
-    "Mercado Cico", "Extra Mercado", "Mercado Azevedo", "Hirota Food", "DIA Supermercado", "Vencedor Atacadista", "Bom Fran Mercado",
-    "SuperFoods Brasil", "Natural da Terra", "Fispal Food Service", "Naturaltech", "B2Brazil", "Bio Mundo", "Arezzo Alimentos",
-    "Grãos e Cia", "Mercado Brasil Natural", "Natural One", "Viver Melhor", "Vila Granel", "Kopenhagen", "Cacau Show", "Nestlé",
-    "Vovó Benta", "Olist", "Etsy Brasil", "Consultoria Food Network", "ProFood Consulting", "Griletto", "Giraffas", "Burger King",
-    "Nuts & Seeds Trading", "Global Nuts Trading", "Brazil Nuts Export", "Spasso Natural", "Prana Store", "Biospirit", "Empório da Papinha",
-    "Yoki Alimentos", "Camil Alimentos", "Polo Distribuidor", "Mega Atacado", "Ketra Alimentos", "Distribuidora GSC"
+    "EMPÓRIO SETE MESTRES", "Macadâmia Cerealista LTDA", "MACADÂMIA MERCADO SAUDÁVEL", "Macadamia Bolsas e Acessórios", "Macadâmia Home",
+    "Macadâmia moda feminina", "Mr. Cheney - Cookies", "Biscoitê A Biscoiteira", "Dadiva Natural - Loja de Produtos Naturais Santo André",
+    "Macadâmia Bela Vista", "Restaurante Bananeira", "Pecan Restaurante - Shop. Paineiras", "Residencial Pecan", "Pecan Móveis Planejados",
+    "Loteamento Fechado Pecan (Lotes a partir de 360 M2)", "Pecan Store", "Pé de Caju Bar & Petiscaria", "Caju Bar", "CaJu Lanches e Salgados",
+    "Boteco do Juca", "Empório Caju - Cerealista", "Restaurante do Caju", "Lanchonete e Restaurante Caju Verde", "Restaurante Caju Verde",
+    "Lanchonete e Restaurante Caju", "Tapiocas e pastéis gourmet do cajú", "Vila Caju", "Lanchonete Nova Skina Caju Verde", "Lanchonete e Restaurante Toca do Caju",
+    "Maria Caju", "CAJU MEL DOCES", "Caju açai e lanches", "Atacadão - São Bernardo Centro", "Web Atacados", "Vencedor Atacadista", "Atacado Bem Barato",
+    "Assaí Atacado e Varejo", "Mafat atacado de roupas semi novas e variedades", "Tenda Atacado - Diadema", "D&S Atacado e Varejo Infantil", "Grupo Bem Barato",
+    "Atacado de folheados", "BRITO & CASONATO Distribuidora de TNT (tecido-nao-tecido)", "Atento atacadista", "Mega Boi", "L Modas Jeans Atacado e Varejo",
+    "Utensílios e Utilidades Domésticas Atacado e Varejo Estrela Das Compras", "Carne Atacado", "Formosa Atacado", "Reis Atacado", "J.A modas e Lingeries atacado e varejo",
+    "Oba Hortifruti Farm - Mercado, Frutas, Verduras, Legumes, Açougue e Adega", "Hirota Food", "Mercado DuParque ll", "DIA Supermercado", "Extra Mercado",
+    "MinhaAmazonBR", "May 13", "Almeida Leme market", "Mercado Lucas", "Mercado Jupiter 1", "Bom Fran Mercado", "Mercado Cico", "Mercado Loyola", "Supermercado Sambelar",
+    "Mercado Bom Preço", "Mercado Azevedo", "EMPÓRIO ALEX - SÃO BERNARDO PLAZA SHOPPING", "Empório Prime", "Empório do Amâncio", "Empório Marcon - Padaria em São Bernardo",
+    "Empório Eliete", "Restaurante Empório Demarchi no Centro", "EMPÓRIO BLACK BAETA", "Barbecue is the name", "Empório do Miro", "Emporio abadejo", "EMPORIO DO ACO",
+    "Empório do Bacalhau", "EmpÓRIO ALEX - SBC - CENTRO", "Emporio Norte Natural", "EmpÓRIO SETE MESTRES", "Empório Esperança", "Empório Uai Berno melhor de Minas SBC",
+    "Empório Canaã", "Empório Grand Prime", "Empório EPB", "7cdrinks - Batidas & caipirinhas", "Marula Artesanal Morin - Licor", "Imigrantes Bebidas", "Magazine",
+    "Empório Frei Caneca", "Cachaçaria Dalic", "Cachaça é Presente", "Emporio Vignamazzi | Center Norte", "Empório Frades", "Licoretto Bebidas", "Adega Do Alê",
+    "Liquore di Famiglia - Licores artesanais", "Drinks distributor Hs", "Mister Bebidas", "Empório Marino , Produtos Importados no ABC", "Adega La Casa das Bebidas",
+    "Vilari Empório de Bebidas", "Instituto Baru", "Baru", "Barú Marisquería", "Baru Restobar", "Baru Restaurante", "Baruk Burger Ferrazópoilis SBC", "Baru Imóveis",
+    "Lojabaru", "BARU Records", "Baru Learn & Play", "Baru Reforma E Construção", "Baru Surf Br", "Baru Trans Transporte Executivo", "Baru Utilidades e Conveniencias loja virtual",
+    "SUCO BAGACO SÃO BERNARDO PLAZA", "Suco Bagaco Shopping ABC", "Suco Bagaço", "Juice station", "Guib's - sucos e pasteis e lanches", "Mister Coco Sucos Naturais",
+    "Santo Suco", "Madureira Sucos Itaim Bibi", "Buona Vita", "GoJuice Natural Food", "To 10 Sucos & Lanches", "Pães Doces Sucos Lanches", "Vilajo Sucos e Lanches",
+    "Canapum Sucos", "Bom Suco Liberdade", "Restaurante e Lanchonete Nova Geração"
 ]
+
 
 
 # Executa a consulta das empresas
